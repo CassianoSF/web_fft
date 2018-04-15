67224af35a840e4c14d2b45e1206bf4b9e154ad1
CassianoSF <cassianosfranco@gmail.com>
2018-04-15 11:47:23 -0300

    git init

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import './App.css';
+import * as fftjs from 'fft-js';
+import {Line} from 'react-chartjs';
+
+
+
+// Use the in-place mapper to populate the data.
+
+class App extends Component {
+  constructor(props) {    
+    super(props);
+
+    let fft = fftjs.fft,
+        fftUtil = fftjs.util
+
+    let xs = [...Array(512).keys()]
+
+    let signal = xs.map(x => Math.sin(0.9*6.28*x) + 0.2*Math.sin(1.2*6.28*x) )
+
+    let phasors= fft(signal);
+
+    let frequencies = fftUtil.fftFreq(phasors, 8000),
+        magnitudes = fftUtil.fftMag(phasors); 
+
+    let both = frequencies.map(function (f, ix) {
+        return {frequency: f, magnitude: magnitudes[ix]};
+    });
+
+    let options = {
+      pointDot: false,
+    }
+
+
+    this.state = {
+          phasors: phasors,
+               xs: xs,
+             both: both,
+           signal: signal,
+      frequencies: frequencies,
+       magnitudes: magnitudes,
+          options: options
+    }
+
+  }
+
+  
+  render() {
+    console.log(this.state)
+    let signal_graph = {
+      labels:  this.state.xs,
+      datasets: [
+        {
+          label: "My First dataset",
+          fillColor: "rgba(220,220,255,0)",
+          strokeColor: "rgba(0,0,255,1)",
+          pointColor: "rgba(0,0,255,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data:  this.state.signal
+        }
+      ]
+    }
+
+    let ft_graph = {
+      labels:  this.state.frequencies,
+      datasets: [
+        {
+          label: "My First dataset",
+          fillColor: "rgba(220,220,255,0)",
+          strokeColor: "rgba(0,0,255,1)",
+          pointColor: "rgba(0,0,255,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data:  this.state.magnitudes
+        }
+      ]
+    }
+    return (
+      <div className="App">
+        <Line data={signal_graph} options={this.state.options} width="1500" height="600"/>
+        <Line data={ft_graph} options={this.state.options} width="1500" height="600"/>
+      </div>
+    );
+  }
+}
+
+export default App;
